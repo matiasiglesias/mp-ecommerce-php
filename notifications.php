f56145ffac170d45e44cd80c9e99fd47e37e5053
Matias Iglesias <miglesias@guanaosoftware.com.ar>
2020-06-11 16:53:02 -0300

    log de notificaciones

--- notifications.php
+++ notifications.php
@@ -1,7 +1,5 @@
 <?
-error_log(sprintf("REQUEST: %s", $_REQUEST));
-
-error_log(sprintf("POST: %s", $_POST));
-
-error_log(sprintf("NOTIFICACION: %s", file_get_contents('php://input')));
+error_log("<-----REQUEST----->" . print_r($_REQUEST, true));
+error_log("<-----POST----->" . print_r($_POST, true));
+error_log("<-----NOTIFICACION----->" . print_r(file_get_contents('php://input'), true));
 ?>
