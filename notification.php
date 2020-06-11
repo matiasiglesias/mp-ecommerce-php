054ac36b51205446c1621147195726d66b13ac44
Matias Iglesias <miglesias@guanaosoftware.com.ar>
2020-06-11 16:48:50 -0300

    notificaciones

--- notification.php
+++
@@ -1,6 +0,0 @@
-<?
-error_log("-------------->  RESPUESTA POST  <------------ ".print_r($_POST, true));
-error_log("-------------->  REQUEST  <------------ ".print_r($_REQUEST, true));
-$entityBody = file_get_contents('php://input');
-error_log("-------------->  RESPUESTA JSON NOTIFICACION  --------------> ".print_r($entityBody, true));
-?>
