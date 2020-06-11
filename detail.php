51d98b21bfa19f0cfde5a5d534e86ea7d3cb09dd
Matias Iglesias <miglesias@guanaosoftware.com.ar>
2020-06-11 15:30:26 -0300

    simplificación

--- detail.php
+++ detail.php
@@ -613,10 +613,8 @@
                                         </h3>
                                     </div>
 
-                                    <form action="/procesar-pago.php" method="POST">
-                                        <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>" data-button-label="Pagar la compra">
-                                        </script>
-                                    </form>
+                                    <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>" data-button-label="Pagar la compra">
+                                    </script>
 
                                 </div>
                             </div>
