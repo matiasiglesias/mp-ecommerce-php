8d40edea0a86da4403ddb633155f0ebc2e565a44
Matias Iglesias <miglesias@guanaosoftware.com.ar>
2020-06-11 16:44:22 -0300

    log de notificacion

--- notification.php
+++ notification.php
@@ -1,7 +1,6 @@
 <?
-error_log(sprintf("REQUEST: %s", $_REQUEST));
-
-error_log(sprintf("POST: %s", $_POST));
-
-error_log(sprintf("NOTIFICACION: %s", file_get_contents('php://input')));
+error_log("-------------->  RESPUESTA POST  <------------ ".print_r($_POST, true));
+error_log("-------------->  REQUEST  <------------ ".print_r($_REQUEST, true));
+$entityBody = file_get_contents('php://input');
+error_log("-------------->  RESPUESTA JSON NOTIFICACION  --------------> ".print_r($entityBody, true));
 ?>
